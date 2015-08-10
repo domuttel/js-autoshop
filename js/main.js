4eef4f4f000fc298add09a8b3451c557f0140c5c
Dominic Muttel <dominic.muttel@gmail.com>
2015-08-09 18:26:42 -0600

    finalized with part 2 complete

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ var Car = function(make, model, year, state, color, current_owner) {
   this.color = color;
   this.previous_owner = [];
   this.current_owner = "manufacturer";
+// add constructors for passengers
+  this.passengers = [];
 };
 
 // added part one methods for Car class
@@ -28,6 +30,7 @@ Car.prototype.start = function() {
 Car.prototype.start = function() {
   this.state = "off";
 };
+
 Car.prototype.driveTo = function(destination) {
   if (this.state === "on") {
     console.log("Driving to " + destination);
@@ -36,11 +39,27 @@ Car.prototype.driveTo = function(destination) {
     console.log("Sorry, this car is off");
   }
 };
+
 Car.prototype.park = function() {
   if (this.state === "off") {
     console.log("Parked!");
   }
   else {
     console.log("Sorry, the car is still on.");
+  }
+};
+
+// add part 2 - properties to constructor
+Car.prototype.pickUp = function(name) {
+  passengers.push(name);
+  if(this.state === "on") {
+  console.log("Driving to pick up " + name);
+  }
+};
+
+Car.prototype.dropOff = function(name) {
+  if(this.state === "on") {
+  var index = passengers.indexOf(name);
+  passengers.splice(index, 1);
   }
 };
